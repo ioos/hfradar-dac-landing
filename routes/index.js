7c21a3814823462380072b7a943c089bfe80e94e
Christopher Adams <chrismichaeladams@gmail.com>
2018-09-17 10:28:01 -0400

    Add GA tracking and bump version to 0.0.3p4

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+/* For development, load local .env file */
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IOOS HF Radar' });
+  res.render('index', { title: 'IOOS HF Radar', GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID });
 });
 
 module.exports = router;
